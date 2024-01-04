e94a5c789cf0ce9ee7e305533b27904d31d902a3
naol86 <lijnaolkasinet@gmail.com>
2024-01-04 16:25:31 +0300

    test

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -11,14 +11,15 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (new_node == NULL)
 		return (NULL);
 	new_node = binary_tree_node(parent, value);
-	if (parent->right == NULL)
-		parent->right = new_node;
-	else
+
+	if (parent->right != NULL)
 	{
 		new_node->right = parent->right;
 		parent->right->parent = new_node;
 		parent->right = new_node;
 	}
+
+	parent->right = new_node;
 
 	return (new_node);
 }
